bc5fc6d354c27d443907d0b7a753593f93d6e683
Martin Odstrčilík <martin.odstrcilik@lablabs.io>
2026-01-16 12:57:01 +0100

    feat(addon)!: add support for addon depends on (#22)
    # Description
    
    <!--
    Please include a summary of the change, provide a justification and
    which issue is fixed.
    -->
    
    :warning: **[BREAKING]** In this PR addon module version is bumped to
    0.0.25 to include the new feature in the addon. The most significant
    change is Terraform 1.8 requirement.
    
    Additionally, this PR adds variable *addon_depends_on* to have more
    granular control over addon dependencies preventing Terraform plan to
    trigger change when data source changes.
    
    ## Type of change
    
    - [ ] A bug fix (PR prefix `fix`)
    - [x] A new feature (PR prefix `feat`)
    - [ ] A code change that neither fixes a bug nor adds a feature (PR
    prefix `refactor`)
    - [ ] Adding missing tests or correcting existing tests (PR prefix
    `test`)
    - [ ] Changes that do not affect the meaning of the code like
    white-spaces, formatting, missing semi-colons, etc. (PR prefix `style`)
    - [ ] Changes to our CI configuration files and scripts (PR prefix `ci`)
    - [ ] Documentation only changes (PR prefix `docs`)
    
    ## How Has This Been Tested?
    
    <!--
    Please describe the tests that you ran to verify your changes.
    -->
    
    Checked when *addon_depends_on* is used instead of *depends_on* if the
    Terraform plan stays the same when dependency changes.

--- variables.tf
+++ variables.tf
@@ -1,11 +1,4 @@
 # IMPORTANT: Add addon specific variables here
-variable "enabled" {
-  type        = bool
-  default     = true
-  description = "Set to false to prevent the module from creating any resources."
-  nullable    = false
-}
-
 variable "grafana_admin_user" {
   type        = string
   default     = "admin"
